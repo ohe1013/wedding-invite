e5fe43cbdced297cf88bff5e4b457c7452e4ece8
oh hyun keun <dhgusrms23@naver.com>
2025-03-23 11:20:17 +0900

    build error

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { NavermapsProvider } from 'react-naver-maps';
 import { ToastContainer, Zoom } from 'react-toastify';
-import Button from '@/components/Button.tsx';
+// import Button from '@/components/Button.tsx';
 import { Heading1 } from '@/components/Text.tsx';
 import Wrapper from '@/components/Wrapper.tsx';
 import Account from '@/layout/Account/Account.tsx';
